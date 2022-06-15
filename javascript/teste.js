23f3c15243fbfecff66a751f193ccfdedc84661f
Antonio <antoniogrneto@hotmail.com>
2022-06-15 18:07:18 -0300

    Organização das pastas do projeto

---
+++ javascript/teste.js
@@ -0,0 +1,13 @@
+// var y = 0;
+/*var x = 1;
+comentar diversas linhas/*
+
+/* BOAS PRÁTICAS:
+    variável: letras pequenas, sem acento, nomes que façam sentido;
+    constante: CAIXA ALTA, mesmo esquema.
+*/
+
+let = 1;
+
+console.log("My ", let, "º Javascript line!!")
+
